350c14f433668c25f5a06d5c6976605a95a6d92a
Анастасия Паневская <AnastasiaPanevskaya@yandex.by>
2024-09-14 11:30:13 +0300

    new

---
+++ Spiski/29.py
@@ -0,0 +1,6 @@
+# Дополни приведенный код списочным выражением, чтобы получить новый список,
+# содержащий только слова длиной не менее пяти символов (включительно).
+
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 
+            'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 
+            'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
