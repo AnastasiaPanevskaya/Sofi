35f73ccd77e6035336b4bf7fcdea8a08b9250d1a
Анастасия Паневская <AnastasiaPanevskaya@yandex.by>
2024-09-21 17:40:34 +0300

    func

---
+++ Func/4.py
@@ -0,0 +1,25 @@
+# Напиши функцию print_fio(name, surname, patronymic), которая принимает три параметра:
+# name – имя человека;
+# surname – фамилия человека;
+# patronymic – отчество человека;
+# а затем выводит на печать ФИО человека
+# Предусмотрите тот факт, что все три буквы в ФИО должны иметь верхний регистр
+# input
+# тимур
+# Гуев
+# ахсарбекович
+# output
+# ГТА
+
+
+
+
+# объявление функции
+def print_fio(name, surname, patronymic):
+    pass
+
+# считываем данные
+name, surname, patronymic = input(), input(), input()
+
+# вызываем функцию
+print_fio(name, surname, patronymic)
