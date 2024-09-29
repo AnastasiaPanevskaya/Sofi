36ab590906f7d5d409e196d6b484359c74038894
Анастасия Паневская <AnastasiaPanevskaya@yandex.by>
2024-09-29 11:36:12 +0300

    new_func

---
+++ Func/17.py
@@ -0,0 +1,14 @@
+# Напиши функцию convert_to_python_case(text), которая принимает в качестве аргумента строку
+# в «верблюжьем регистре» и преобразует его в «змеиный регистр».
+# print(convert_to_python_case('ThisIsCamelCased'))
+# this_is_camel_cased
+
+# объявление функции
+def convert_to_python_case(text):
+    pass
+
+# считываем данные
+txt = input()
+
+# вызываем функцию
+print(convert_to_python_case(txt))
